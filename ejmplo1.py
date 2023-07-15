df1aaac9ceabeac4bd84c3a61d4b032f2a071e3c
developer <jjquimal@alu.upo.es>
2023-07-15 21:31:18 +0200

    inicio de proyecto

---
+++ ejmplo1.py
@@ -0,0 +1,13 @@
+
+
+
+def salutation():
+
+    print("Bonjour le monde")
+
+listeQuelquesMots1=('Bonjour','Au revoir','Merci',"S'il vous plaît")
+
+
+def montrer_le_mot(index):
+    
+    print(listeQuelquesMots1[index])
